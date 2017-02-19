48969b4ceaecddc2e06b0da4f938c33361a7a5d5
Matvey Stepanov <matthewz@yandex.ru>
2017-02-20 02:33:41 +0300

    REFACTORED FibonacciSequencesManager messages contain FibonacciState

--- PositiveTechnologies/FibonacciSequencesManager.cs
+++ PositiveTechnologies/FibonacciSequencesManager.cs
@@ -8,73 +8,77 @@ namespace PositiveTechnologies
     {
         public int Count { get; private set; }
         public Func<FibonacciState, IFibonacciSequence> SequencerCreator { get; private set; }
+        public FibonacciState FirstState { get; private set; }
+        public FibonacciState SecondState { get; private set; }
 
-        public FibonacciSequencesManager(int count, Func<FibonacciState, IFibonacciSequence> sequencerCreator)
+        public FibonacciSequencesManager(int count, Func<FibonacciState, IFibonacciSequence> sequencerCreator, FibonacciState firstState, FibonacciState secondState)
         {
             Count = count;
             SequencerCreator = sequencerCreator;
+            FirstState = firstState;
+            SecondState = secondState;
         }
 
-        private readonly BufferBlock<UpdateEvent> _outPort = new BufferBlock<UpdateEvent>();
-        public ISourceBlock<UpdateEvent> OutPort { get { return _outPort; } }
+        private readonly BufferBlock<UpdateMessage> _outPort = new BufferBlock<UpdateMessage>();
+        public ISourceBlock<UpdateMessage> OutPort { get { return _outPort; } }
 
-        private readonly BufferBlock<UpdateEvent> _inPort = new BufferBlock<UpdateEvent>();
-        public ITargetBlock<UpdateEvent> InPort { get { return _inPort; } }
+        private readonly BufferBlock<UpdateMessage> _inPort = new BufferBlock<UpdateMessage>();
+        public ITargetBlock<UpdateMessage> InPort { get { return _inPort; } }
 
         public void Init()
         {
             foreach (var seqId in Enumerable.Range(1, Count))
-                LinkUpdateSequenceBlock(CreateUpdateSequenceBlock(0), seqId);
+                LinkUpdateSequenceBlock(CreateUpdateSequenceBlock(FirstState), seqId);
 
             LinkAddNewSequencerBlock(CreateAddNewSequencerBlock());
 
             foreach (var seqId in Enumerable.Range(1, Count))
-                _outPort.Post(new UpdateEvent { SequenceId = seqId, State = 0 });
+                _outPort.Post(new UpdateMessage { SequenceId = seqId, State = FirstState });
         }
 
-        private void LinkAddNewSequencerBlock(ITargetBlock<UpdateEvent> addNewSequencerBlock)
+        private void LinkAddNewSequencerBlock(ITargetBlock<UpdateMessage> addNewSequencerBlock)
         {
-            _inPort.LinkTo(addNewSequencerBlock, new DataflowLinkOptions {MaxMessages = 1}, ue => ue.State == 0);
+            _inPort.LinkTo(addNewSequencerBlock, new DataflowLinkOptions { MaxMessages = 1 }, um => um.State.Equals(FirstState));
         }
 
-        private void LinkUpdateSequenceBlock(IPropagatorBlock<UpdateEvent, UpdateEvent> updateSequenceBlock, int seqId)
+        private void LinkUpdateSequenceBlock(IPropagatorBlock<UpdateMessage, UpdateMessage> updateSequenceBlock, int seqId)
         {
-            _inPort.LinkTo(updateSequenceBlock, ue => ue.SequenceId == seqId);
+            _inPort.LinkTo(updateSequenceBlock, um => um.SequenceId == seqId);
             updateSequenceBlock.LinkTo(_outPort);
         }
 
-        private TransformBlock<UpdateEvent, UpdateEvent> CreateUpdateSequenceBlock(int state)
+        private TransformBlock<UpdateMessage, UpdateMessage> CreateUpdateSequenceBlock(FibonacciState state)
         {
-            var sequencer = SequencerCreator(new FibonacciState(state));
-            return new TransformBlock<UpdateEvent, UpdateEvent>(ue => new UpdateEvent
+            var sequencer = SequencerCreator(state);
+            return new TransformBlock<UpdateMessage, UpdateMessage>(um => new UpdateMessage
                 {
-                    SequenceId = ue.SequenceId,
-                    State = sequencer.Next(new FibonacciState(ue.State)).Value
+                    SequenceId = um.SequenceId,
+                    State = sequencer.Next(um.State)
                 });
         }
 
-        private ActionBlock<UpdateEvent> CreateAddNewSequencerBlock()
+        private ActionBlock<UpdateMessage> CreateAddNewSequencerBlock()
         {
-            ActionBlock<UpdateEvent> addNewSequencer = null;
-            addNewSequencer = new ActionBlock<UpdateEvent>(ue =>
+            ActionBlock<UpdateMessage> addNewSequencer = null;
+            addNewSequencer = new ActionBlock<UpdateMessage>(um =>
             {
-                LinkUpdateSequenceBlock(CreateUpdateSequenceBlock(1), ue.SequenceId);
+                LinkUpdateSequenceBlock(CreateUpdateSequenceBlock(SecondState), um.SequenceId);
                 Count++;
 
                 LinkAddNewSequencerBlock(addNewSequencer);
-                _outPort.Post(new UpdateEvent { SequenceId = ue.SequenceId, State = 1 });
+                _outPort.Post(new UpdateMessage { SequenceId = um.SequenceId, State = SecondState });
             });
             return addNewSequencer;
         }
 
-        public struct UpdateEvent
+        public struct UpdateMessage
         {
             public int SequenceId { get; set; }
-            public int State { get; set; }
+            public FibonacciState State { get; set; }
 
             public override string ToString()
             {
-                return String.Format("Sequence {0}, state - {1}", SequenceId, State);
+                return String.Format("Sequence {0}, state - {1}", SequenceId, State.Value);
             }
         }
     }
--- PositiveTechnologies/FibonacciSequencesManagerTests.cs
+++ PositiveTechnologies/FibonacciSequencesManagerTests.cs
@@ -10,56 +10,59 @@ namespace PositiveTechnologies
 {
     public class FibonacciSequencesManagerTests
     {
+        readonly FibonacciState FS = new FibonacciState(0);
+        readonly FibonacciState SS = new FibonacciState(1);
+
         [Test]
         public void Init()
         {
             var sequencerCreator = Substitute.For<Func<FibonacciState, IFibonacciSequence>>();
-            var fcm = new FibonacciSequencesManager(2, sequencerCreator);
+            var fcm = new FibonacciSequencesManager(2, sequencerCreator, FS, SS);
 
             fcm.Init();
             
             Assert.That(fcm.Count, Is.EqualTo(2));
 
-            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateEvent { SequenceId = 1, State = 0 }));
-            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateEvent { SequenceId = 2, State = 0 }));
+            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateMessage { SequenceId = 1, State = FS }));
+            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateMessage { SequenceId = 2, State = FS }));
             sequencerCreator.Received(2).Invoke(new FibonacciState(0));
 
-            fcm.InPort.Post(new FibonacciSequencesManager.UpdateEvent { SequenceId = 1, State = 1 });
+            fcm.InPort.Post(new FibonacciSequencesManager.UpdateMessage { SequenceId = 1, State = SS });
             Assert.That(fcm.OutPort.ReceiveWithTimeout().SequenceId, Is.EqualTo(1));
-            fcm.InPort.Post(new FibonacciSequencesManager.UpdateEvent { SequenceId = 2, State = 1 });
+            fcm.InPort.Post(new FibonacciSequencesManager.UpdateMessage { SequenceId = 2, State = SS });
             Assert.That(fcm.OutPort.ReceiveWithTimeout().SequenceId, Is.EqualTo(2));
-            fcm.InPort.Post(new FibonacciSequencesManager.UpdateEvent { SequenceId = 3, State = 1 });
+            fcm.InPort.Post(new FibonacciSequencesManager.UpdateMessage { SequenceId = 3, State = SS });
             Assert.Throws<TimeoutException>(() => fcm.OutPort.ReceiveWithTimeout());
         }
 
         [Test]
-        public void InitEventHandling()
+        public void InitMessageHandling()
         {
             var sequencerCreator = Substitute.For<Func<FibonacciState, IFibonacciSequence>>();
-            var fcm = new FibonacciSequencesManager(0, sequencerCreator);
+            var fcm = new FibonacciSequencesManager(0, sequencerCreator, FS, SS);
             fcm.Init();
 
-            fcm.InPort.Post(new FibonacciSequencesManager.UpdateEvent { SequenceId = 5, State = 0 });
+            fcm.InPort.Post(new FibonacciSequencesManager.UpdateMessage { SequenceId = 5, State = FS });
 
-            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateEvent { SequenceId = 5, State = 1 }));
+            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateMessage { SequenceId = 5, State = SS }));
             Assert.That(fcm.Count, Is.EqualTo(1));
             sequencerCreator.Received().Invoke(new FibonacciState(1));
 
-            fcm.InPort.Post(new FibonacciSequencesManager.UpdateEvent { SequenceId = 2, State = 0 });
+            fcm.InPort.Post(new FibonacciSequencesManager.UpdateMessage { SequenceId = 2, State = FS });
 
-            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateEvent { SequenceId = 2, State = 1 }));
+            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateMessage { SequenceId = 2, State = SS }));
             Assert.That(fcm.Count, Is.EqualTo(2));
             sequencerCreator.Received(2).Invoke(new FibonacciState(1));
         }
 
         [Test]
         [Ignore("Not implemented")]
-        public void InitEventHandling_AlreadyInited()
+        public void InitMessageHandling_AlreadyInited()
         {
         }
 
         [Test]
-        public void UpdateEventHandling()
+        public void UpdateMessageHandling()
         {
             var sequencer1 = Substitute.For<IFibonacciSequence>();
             sequencer1.Next(Arg.Any<FibonacciState>()).Returns(new FibonacciState(100));
@@ -68,28 +71,28 @@ namespace PositiveTechnologies
             
             var sequencerCreator = Substitute.For<Func<FibonacciState, IFibonacciSequence>>();
             sequencerCreator.Invoke(Arg.Any<FibonacciState>()).Returns(sequencer1, sequencer2);
-                
-            var fcm = new FibonacciSequencesManager(2, sequencerCreator);
+
+            var fcm = new FibonacciSequencesManager(2, sequencerCreator, FS, SS);
             fcm.Init();
 
             //Flush initial messages
             fcm.OutPort.Receive();
             fcm.OutPort.Receive();
 
-            fcm.InPort.Post(new FibonacciSequencesManager.UpdateEvent { SequenceId = 1, State = 50 });
+            fcm.InPort.Post(new FibonacciSequencesManager.UpdateMessage { SequenceId = 1, State = new FibonacciState(50) });
             
-            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateEvent { SequenceId = 1, State = 100 }));
+            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateMessage { SequenceId = 1, State = new FibonacciState(100) }));
             sequencer1.Received().Next(new FibonacciState(50));
 
-            fcm.InPort.Post(new FibonacciSequencesManager.UpdateEvent { SequenceId = 2, State = 500 });
+            fcm.InPort.Post(new FibonacciSequencesManager.UpdateMessage { SequenceId = 2, State = new FibonacciState(500) });
 
-            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateEvent { SequenceId = 2, State = 1000 }));
+            Assert.That(fcm.OutPort.ReceiveWithTimeout(), Is.EqualTo(new FibonacciSequencesManager.UpdateMessage { SequenceId = 2, State = new FibonacciState(1000) }));
             sequencer2.Received().Next(new FibonacciState(500));
         }
 
         [Test]
         [Ignore("Not implemented")]
-        public void UpdateEventHandling_UninitializedSequence()
+        public void UpdateMessageHandling_UninitializedSequence()
         {
         }
 
@@ -97,12 +100,12 @@ namespace PositiveTechnologies
         public void Integration()
         {
             Func<FibonacciState, IFibonacciSequence> seqCreator = s => new FibonacciSkipOneSequence(s);
-            var agent1 = new FibonacciSequencesManager(2, seqCreator);
-            var agent2 = new FibonacciSequencesManager(0, seqCreator);
+            var agent1 = new FibonacciSequencesManager(2, seqCreator, FS, SS);
+            var agent2 = new FibonacciSequencesManager(0, seqCreator, FS, SS);
 
-            var messageLog = new List<FibonacciSequencesManager.UpdateEvent>();
+            var messageLog = new List<FibonacciSequencesManager.UpdateMessage>();
 
-            var logBlock1 = new TransformBlock<FibonacciSequencesManager.UpdateEvent, FibonacciSequencesManager.UpdateEvent>(
+            var logBlock1 = new TransformBlock<FibonacciSequencesManager.UpdateMessage, FibonacciSequencesManager.UpdateMessage>(
                 ue => { lock (messageLog) {
                         messageLog.Add(ue); return ue;
                     }
@@ -110,7 +113,7 @@ namespace PositiveTechnologies
             agent1.OutPort.LinkTo(logBlock1);
             logBlock1.LinkTo(agent2.InPort);
 
-            var logBlock2 = new TransformBlock<FibonacciSequencesManager.UpdateEvent, FibonacciSequencesManager.UpdateEvent>(
+            var logBlock2 = new TransformBlock<FibonacciSequencesManager.UpdateMessage, FibonacciSequencesManager.UpdateMessage>(
                 ue => { lock (messageLog) {
                         messageLog.Add(ue); return ue;
                     }
@@ -123,9 +126,9 @@ namespace PositiveTechnologies
 
             Thread.Sleep(100);
 
-            Assert.That(messageLog.ToArray().Where(ue => ue.SequenceId == 1).Select(ue => ue.State).Take(10).ToList(),
+            Assert.That(messageLog.ToArray().Where(ue => ue.SequenceId == 1).Select(ue => ue.State.Value).Take(10).ToList(),
                 Is.EqualTo(new[] {0, 1, 1, 2, 3, 5, 8, 13, 21, 34}));
-            Assert.That(messageLog.ToArray().Where(ue => ue.SequenceId == 2).Select(ue => ue.State).Take(10).ToList(),
+            Assert.That(messageLog.ToArray().Where(ue => ue.SequenceId == 2).Select(ue => ue.State.Value).Take(10).ToList(),
                 Is.EqualTo(new[] {0, 1, 1, 2, 3, 5, 8, 13, 21, 34}));
         }
     }
